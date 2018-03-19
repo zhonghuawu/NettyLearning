13bfe0be7d42887c7647dc8909a690bc856994e0
zhonghuawu <wu_zhonghua@outlook.com>
2018-03-19 20:16:19 +0800

    update c4.2.1 and c4.3.1 and c4.3.2

--- src/com/huaa/netty/c4_2_1/TimeClient.java
+++ src/com/huaa/netty/c4_2_1/TimeClient.java
@@ -1,4 +1,4 @@
-package com.huaa.netty.c4_3_2;
+package com.huaa.netty.c4_2_1;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
@@ -7,8 +7,6 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.LineBasedFrameDecoder;
-import io.netty.handler.codec.string.StringDecoder;
 
 public class TimeClient {
 
@@ -31,8 +29,6 @@ public class TimeClient {
 					@Override
 					protected void initChannel(SocketChannel ch) throws Exception {
 						// TODO Auto-generated method stub
-						ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
-						ch.pipeline().addLast(new StringDecoder());
 						ch.pipeline().addLast(new TimeClientHandler());
 					}
 				});
