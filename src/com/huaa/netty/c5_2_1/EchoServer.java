4d831d0326e7a23fe662eef7b0d05a2bfb36a007
zhonghuawu <wu_zhonghua@outlook.com>
2018-03-19 22:01:41 +0800

    remove used import

--- src/com/huaa/netty/c5_2_1/EchoServer.java
+++ src/com/huaa/netty/c5_2_1/EchoServer.java
@@ -1,8 +1,6 @@
 package com.huaa.netty.c5_2_1;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
@@ -10,7 +8,6 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.FixedLengthFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.logging.LogLevel;
